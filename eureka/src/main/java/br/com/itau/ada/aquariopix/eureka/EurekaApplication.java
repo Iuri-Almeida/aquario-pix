3593e74222492c10feeb32ad621b05bfd21a1bd1
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-12-09 16:35:31 -0300

    feat(eureka): add Eureka configurations

---
+++ eureka/src/main/java/br/com/itau/ada/aquariopix/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package br.com.itau.ada.aquariopix.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
