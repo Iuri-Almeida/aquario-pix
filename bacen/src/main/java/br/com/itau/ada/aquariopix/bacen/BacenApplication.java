dad198114248cb4cf86e6d3c012ee323deda52b1
Ellen Cintra Monteiro <ellenc.monteiro@gmail.com>
2022-12-12 15:13:26 -0300

    adicionando Bacen Service e suas dependências - depois configurar application properties

---
+++ bacen/src/main/java/br/com/itau/ada/aquariopix/bacen/BacenApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.ada.aquariopix.bacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacenApplication.class, args);
+	}
+
+}
