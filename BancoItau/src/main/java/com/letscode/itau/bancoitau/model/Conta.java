c2cfaf25efcb0485c944ed599afb3dda433fbf8b
Bruno <b.augusto1501@gmail.com>
2022-12-13 19:18:06 -0300

    script sql tabela conta

--- BancoItau/src/main/java/com/letscode/itau/bancoitau/model/Conta.java
+++ BancoItau/src/main/java/com/letscode/itau/bancoitau/model/Conta.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table("CONTA")
 public class Conta {
 
     @Id
--- BancoItau/src/main/resources/schema.sql
+++ BancoItau/src/main/resources/schema.sql
@@ -1,10 +1,19 @@
 create table PIX(
-    id bigint AUTO_INCREMENT,
-    reqId bigint,
+    id bigint AUTO_INCREMENT unique ,
+    reqId bigint unique ,
     tipo varchar,
     agencia varchar,
     conta varchar,
-    chave varchar primary key,
+    chave varchar primary key not null ,
     status varchar
 
+);
+
+create table CONTA(
+    id bigint AUTO_INCREMENT unique ,
+    nome varchar,
+    cpf varchar primary key not null ,
+    email varchar unique ,
+    numeroConta varchar,
+    agencia varchar
 );
