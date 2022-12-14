b85070578d0b810a4cabfef2eae31b519af11ef1
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-12-14 15:10:36 -0300

    feat(itau): add Eureka Client connection

--- BancoItau/src/main/java/com/letscode/itau/bancoitau/BancoItauApplication.java
+++ BancoItau/src/main/java/com/letscode/itau/bancoitau/BancoItauApplication.java
@@ -3,11 +3,12 @@ package com.letscode.itau.bancoitau;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BancoItauApplication {
 
     public static void main(String[] args) {
@@ -21,7 +22,5 @@ public class BancoItauApplication {
                 .replicas(1)
                 .build();
     }
-
-
 
 }
