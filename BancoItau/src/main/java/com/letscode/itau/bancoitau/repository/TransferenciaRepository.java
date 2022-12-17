e10e7ba5e00fc7924dd83287d3267ed7abf942a1
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-12-17 13:41:40 -0300

    feat(itau): add findByReqId method

--- BancoItau/src/main/java/com/letscode/itau/bancoitau/repository/TransferenciaRepository.java
+++ BancoItau/src/main/java/com/letscode/itau/bancoitau/repository/TransferenciaRepository.java
@@ -7,4 +7,6 @@ import reactor.core.publisher.Mono;
 
 public interface TransferenciaRepository extends R2dbcRepository<PixTransferencia, String> {
 
+    Mono<PixTransferencia> findByReqId(String reqId);
+
 }
