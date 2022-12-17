89946b6bbefade8f859f9e7e492daba8d33fe3b3
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-12-17 14:57:24 -0300

    feat(itau): add ContaBacen model

---
+++ BancoItau/src/main/java/com/letscode/itau/bancoitau/model/ContaBacen.java
@@ -0,0 +1,15 @@
+package com.letscode.itau.bancoitau.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ContaBacen {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String numeroConta;
+    private String agencia;
+    private String banco;
+
+}
