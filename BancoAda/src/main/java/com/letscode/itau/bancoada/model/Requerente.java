c7e65abebe498c833a04b0039cb31b3b4a13c6c8
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-12-19 22:21:13 -0300

    refactor(ada): refactor Requerente model

--- BancoAda/src/main/java/com/letscode/itau/bancoada/model/Requerente.java
+++ BancoAda/src/main/java/com/letscode/itau/bancoada/model/Requerente.java
@@ -1,9 +1,13 @@
 package com.letscode.itau.bancoada.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 //import org.hibernate.validator.constraints.br.CPF;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Requerente {
     private String conta;
     private String agencia;
